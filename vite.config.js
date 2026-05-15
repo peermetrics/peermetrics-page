fcd8ef316b03104ba913d7048f1d261353cfc805
Alberto <alberto@albertos-mini.lan>
2026-05-15 13:49:06 -0400

    Fix GitHub Pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+
 export default defineConfig({
+  base: isGithubPages ? '/peermetrics-page/' : './',
   server: {
     host: true,
   },
